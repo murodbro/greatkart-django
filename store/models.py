1d19ae749c4e7398cbbc69f74feb7f5104fdc0c7
Murodjon <murodjon.work@gmail.com>
2023-10-30 00:11:10 -0700

    initial commit

---
+++ store/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.urls import reverse
+from category.models import Category
+
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255, unique=True)
+    slug = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    images = models.ImageField(upload_to='photos/products')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.product_name
+    
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
